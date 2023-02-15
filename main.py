3e1bfaaf46b19a2ff4c9ac17be64879def66fb7a
Hsutra <96309579+Hsutra@users.noreply.github.com>
2023-02-15 17:06:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,124 @@
+import string
+import itertools
+import random
+import math
+
+def genvector(n, p):
+    vector = list()
+    vector.append(random.randint(1, 10))
+    for i in range(n-1):
+        sum = 0
+        for j in range(len(vector)):
+            sum += vector[j] * (p - 1)
+        vector.append(random.randint(sum + 1, sum + 10))
+    return vector
+
+def gensec(vector):
+    sum = 0
+    for i in vector:
+        sum += i
+    m = random.randint(sum + 1, sum + 10)#модуль(должен быть больше суммы элементов вектора)
+    nlist = []
+    for i in range(2, m):
+        if (math.gcd(m, i) == 1):
+            nlist.append(i)
+    e = random.choice(nlist)#множитель(e,m) = 1
+    return m, e
+
+def gencodes(n, p, a):
+    num_system = []
+    for i in range(p):
+        num_system.append(i)
+    Codes = []
+    set = list(itertools.product(num_system, repeat=n))
+    for i in set:
+        sum = 0
+        k = 1
+        for j in i:
+            sum += k * j
+            k += 1
+        if (sum % (n + 1) == a):
+            Codes.append(i)
+    Alphabet = list(" " + string.ascii_letters + string.digits + string.punctuation)
+    CodeWords = {}
+    random.shuffle(Codes)
+    j = 0
+    if (len(Codes) != 0):
+        for i in Alphabet:
+            CodeWords[i] = Codes[j]
+            j += 1
+    return CodeWords
+
+def encrypt(plane_text, vectorA, CodeWords):
+    shifr = []
+    sum = 0
+    for i in plane_text:
+        for key, value in CodeWords.items():
+            if (i == key):
+                sum = 0
+                k = 0
+                for num in value:
+                    sum += vectorA[k] * num
+                    k += 1
+        shifr.append(sum)
+    return shifr
+
+def decrypt(shifr_text, vectorB, CodeWords, m, e):
+    str = str_to_int(shifr_text)
+    txt = ""
+    u = 0
+    for i in range(m - 1):
+        if ((e * i) % m) == 1:
+            u = i  # u = e^-1
+            break
+    vectorC = []
+    for i in range(len(vectorB)):
+        vectorC.append((vectorB[i] * u) % m)
+    for i in str:
+        for key, value in CodeWords.items():
+            sum = 0
+            k = 0
+            for num in value:
+                sum += vectorC[k] * num
+                k += 1
+            if (sum == i):
+                txt += key
+                break
+    return txt
+
+def str_to_int(str):
+    l = len(str)
+    integ = []
+    i = 0
+    while i < l:
+        s_int = ''
+        a = str[i]
+        while '0' <= a <= '9':
+            s_int += a
+            i += 1
+            if i < l:
+                a = str[i]
+            else:
+                break
+        i += 1
+        if s_int != '':
+            integ.append(int(s_int))
+    return integ
+
+n = 6# n >= 3
+p = 3
+a = 0#должен быть меньше либо равен n
+CodeWords = gencodes(n, p, a)
+VectorA = genvector(n, p)#элемент сверхрастущего вектора должен быть больше суммы всех предыдущих * р - 1
+m, e = gensec(VectorA)
+print("Модуль/Множитель: ", m, e)
+VectorB = []
+for i in range(len(VectorA)):
+    VectorB.append(((VectorA[i] * e) % m))
+print("Закрытый ключ:", VectorA)
+print("Открытый ключ:", VectorB)
+plane_text = "CODE RRW BAC AB AB"
+shifr_text = str(encrypt(plane_text, VectorA, CodeWords))
+print("Открытый текст: ", plane_text)
+print("Шифр текст: ", shifr_text)
+print("Расшифрованный текст: ", decrypt(shifr_text, VectorB, CodeWords, m, e))
